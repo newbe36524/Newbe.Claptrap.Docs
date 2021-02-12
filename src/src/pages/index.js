28a92191ed4db995fea9d0d5a1213d82a115970f
newbe36524 <newbe36524@qq.com>
2021-02-12 11:00:53 +0800

    better home page
    - i18n
    - images

--- src/src/pages/index.js
+++ src/src/pages/index.js
@@ -9,30 +9,13 @@ import Translate, { translate } from "@docusaurus/Translate";
 const features = [
   {
     title: translate({
-      message: "轻松应对并发问题",
-      description: "The homepage 轻松应对并发问题",
-    }),
-    imageUrl: "img/undraw_docusaurus_mountain.svg",
-    description: translate({
-      message: (
-        <>
-          基于 Actor
-          模式和事件溯源模式作为基础原理，轻松处理并发环境下的问题处理。
-        </>
-      ),
-      description: "The homepage 轻松应对并发问题 detail",
-    }),
-  },
-  {
-    title: translate({
       message: "完全支持水平扩展",
       description: "The homepage 完全支持水平扩展",
     }),
-    imageUrl: "img/undraw_docusaurus_tree.svg",
+    imageUrl: "images/scale-out.svg",
     description: translate({
-      message: (
-        <>依托 dapr/Orleans 框架等与生俱来的水平扩展能力，轻松做到从一到百。</>
-      ),
+      message:
+        "依托 dapr/Orleans 框架等与生俱来的水平扩展能力，轻松做到从一到百。",
       description: "The homepage 完全支持水平扩展 detail",
     }),
   },
@@ -41,32 +24,14 @@ const features = [
       message: "全面关注完整周期",
       description: "The homepage 全面关注完整周期",
     }),
-    imageUrl: "img/undraw_docusaurus_react.svg",
+    imageUrl: "images/full-lifetime.svg",
     description: translate({
-      message: (
-        <>
-          从设计，到开发，到上线。每个环节都是我们考虑的要素。每个环节我们都充分考虑。
-        </>
-      ),
+      message:
+        "从设计，到开发，到上线。每个环节都是我们考虑的要素。每个环节我们都充分考虑。",
       description: "The homepage 全面关注完整周期 detail",
     }),
   },
 ];
-
-function Feature({ imageUrl, title, description }) {
-  const imgUrl = useBaseUrl(imageUrl);
-  return (
-    <div className={clsx("col col--4", styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
-        </div>
-      )}
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
-  );
-}
 
 function Home() {
   const context = useDocusaurusContext();
@@ -86,17 +51,84 @@ function Home() {
         </div>
       </header>
       <main>
-        {features && features.length > 0 && (
-          <section className={styles.features}>
-            <div className="container">
-              <div className="row">
-                {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
-                ))}
+        <section className={styles.features}>
+          <div className="container">
+            <div className="row">
+              <div className={clsx("col col--4", styles.feature)}>
+                <div className="text--center">
+                  <img
+                    className={styles.featureImage}
+                    src={useBaseUrl("images/concurrency.svg")}
+                  />
+                </div>
+                <h3>
+                  <Translate
+                    id="homepage.轻松应对并发问题"
+                    description="The homepage 轻松应对并发问题"
+                  >
+                    轻松应对并发问题
+                  </Translate>
+                </h3>
+                <p>
+                  <Translate
+                    id="homepage.轻松应对并发问题.details"
+                    description="The homepage 基于 Actor 模式和事件溯源模式作为基础原理，轻松处理并发环境下的问题处理。"
+                  >
+                    基于 Actor 模式和事件溯源模式作为基础原理，轻松处理并发环境下的问题处理。
+                  </Translate>
+                </p>
+              </div>
+              <div className={clsx("col col--4", styles.feature)}>
+                <div className="text--center">
+                  <img
+                    className={styles.featureImage}
+                    src={useBaseUrl("images/scale-out.svg")}
+                  />
+                </div>
+                <h3>
+                  <Translate
+                    id="homepage.完全支持水平扩展"
+                    description="The homepage 完全支持水平扩展"
+                  >
+                    完全支持水平扩展
+                  </Translate>
+                </h3>
+                <p>
+                  <Translate
+                    id="homepage.完全支持水平扩展.details"
+                    description="The homepage 依托 Dapr/Orleans 框架等与生俱来的水平扩展能力，轻松做到从一到百。"
+                  >
+                    依托 Dapr/Orleans 框架等与生俱来的水平扩展能力，轻松做到从一到百。
+                  </Translate>
+                </p>
+              </div>
+              <div className={clsx("col col--4", styles.feature)}>
+                <div className="text--center">
+                  <img
+                    className={styles.featureImage}
+                    src={useBaseUrl("images/full-lifetime.svg")}
+                  />
+                </div>
+                <h3>
+                  <Translate
+                    id="homepage.全面关注完整周期"
+                    description="The homepage 全面关注完整周期"
+                  >
+                    全面关注完整周期
+                  </Translate>
+                </h3>
+                <p>
+                  <Translate
+                    id="homepage.全面关注完整周期.details"
+                    description="The homepage 从设计，到开发，到上线。每个环节都是我们考虑的要素。每个环节我们都充分考虑。"
+                  >
+                    从设计，到开发，到上线。每个环节都是我们考虑的要素。每个环节我们都充分考虑。
+                  </Translate>
+                </p>
               </div>
             </div>
-          </section>
-        )}
+          </div>
+        </section>
       </main>
     </Layout>
   );
